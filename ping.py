edfb14a9916c15d80be89fa01bbfdce2254a7782
GalinaMash <103889292+GalinaMash@users.noreply.github.com>
2022-04-24 17:13:11 +0300

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,48 @@
+from pygame import *
+from random import randint
+from time import time as timer
+
+win_width = 700
+win_height = 500
+display.set_caption("Ping Pong")
+back=(225, 181, 241)
+window = display.set_mode(( win_width, win_height))
+window.fill(back)
+clock=time.Clock()
+FPS=60
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    # метод для управления спрайтом стрелками клавиатуры
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+
+racket = Player('rocket.png', 5, win_height - 100, 80, 100, 10)
+
+
+
+
+
+run = True # флаг сбрасывается кнопкой закрытия окна
+while run:
+    # событие нажатия на кнопку Закрыть
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        display.update()
+        clock.tick(FPS)
